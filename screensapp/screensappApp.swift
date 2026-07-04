d4c5f73fa394d4dd7c8e575bfbe6a7d95aa80eae
Sunday-Knight01 <kradrake1.0@gmail.com>
2026-07-04 14:22:39 +0800

    Initial Commit

---
+++ screensapp/screensappApp.swift
@@ -0,0 +1,17 @@
+//
+//  screensappApp.swift
+//  screensapp
+//
+//  Created by T Krobot on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct screensappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
